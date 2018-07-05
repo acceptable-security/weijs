41bcdae47c0fa3e812ad55616d9934c1485ea63f
Avi Saven <block8437@gmail.com>
2018-07-05 17:05:00 -0400

    Begun work on getting code coverage but nothign functional yet.

--- test.sh
+++ test.sh
@@ -4,18 +4,19 @@
 eslint --ignore-path .eslintignore .
 
 # Remove old code coverage
-rm -rf src-cov
+# rm -rf src-cov
 
 # Generate new code coverage
-./node_modules/.bin/jscover src src-cov
+# java -jar ./build/JSCover-all.jar -io src src-cov
 
 # Load the covered src
-mv src src-orig
-mv src-cov src
+# mv src src-orig
+# mv src-cov src
 
 # Run mocha and output to coveralls
-./node_modules/mocha/bin/mocha -R mocha-lcov-reporter | ./node_modules/coveralls/bin/coveralls.js
+# ./node_modules/mocha/bin/mocha -R mocha-lcov-reporter | ./node_modules/coveralls/bin/coveralls.js
+./node_modules/mocha/bin/mocha
 
 # Clean up the source
-rm -rf src
-mv src-orig src
+# rm -rf src
+# mv src-orig src
